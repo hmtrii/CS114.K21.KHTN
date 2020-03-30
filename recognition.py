d1be99011de100723abb3b3c1f13b32d9e2b1d19
hmtrii <42197959+hmtrii@users.noreply.github.com>
2020-03-30 20:30:04 +0700

    Update recognition.py

--- recognition.py
+++ recognition.py
@@ -1,14 +1,14 @@
 pip install --upgrade "watson-developer-cloud>=2.4.1"
 pip install simplejson
-import json
 
+import json
 from watson_developer_cloud import VisualRecognitionV3
 
 visual_recognition = VisualRecognitionV3(
     '2018-03-19',
     iam_apikey='1ILZkyzgNMH6FTgPJ8WtT6WR4scQj_LNmkHRFsMN78Pj')
 
-with open('/content/image4.png', 'rb') as images_file:
+with open('/images/image1', 'rb') as images_file:
     classes = visual_recognition.classify(
         images_file,
         threshold='0.6',
